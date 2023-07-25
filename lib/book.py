56529a5efb36173abc719a1b9ff99b5dfa928ab5
Aaron <arnhsmith@gmail.com>
2023-07-25 18:05:08 -0400

    checked code and passed all tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,36 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title, page_count=50):
+        self.title = title
+        self.page_count = page_count
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, title):
+        self._title = title
+
+    title = property(get_title, set_title)
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    page_count = property(get_page_count, set_page_count)
+
+    # this part of the code is complete bullshit and they need to fix it. It should say explicitly print not output
+    def turn_page(self):
+            print("Flipping the page...wow, you read fast!")
+
     
-        
+
+The_Hobbit = Book("The Hobbit", 1000)
+print(The_Hobbit.title, The_Hobbit.page_count)
+
+result = The_Hobbit.turn_page()
+print(result)  
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    # def get_title(self):
+    #     return self._title
+
+    # def set_title(self, title):
+    #     self._title = title
+
+    # title = property(get_title, set_title)
+
+    # def set_page_count(self, page_count):
+    #     if isinstance(page_count, int):
+    #         self._page_count = page_count
+    #     else:
+    #         print("page_count must be an integer")
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        setattr(self, "condition", "New")
+
     pass
+
+shoe = Shoe("Ford", 12)
+shoe.cobble()
